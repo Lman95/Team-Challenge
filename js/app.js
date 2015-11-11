b6b60e4d6748b85fa34412a7c761b509dced8f96
Landon Young <young518@uw.edu>
2015-11-10 20:48:56 -0800

    Add password validation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
 
 angular.module('TeamApp', [])
     .controller('TeamCtrl', ['$scope', '$http', function($scope, $http) {
-
+        $scope.checkPass = function() {
+            if($scope.psw === $scope.psw2){
+                $scope.newForm.psw2.$setValidity('psw2', true);
+            } else {
+                $scope.newForm.psw2.$setValidity('psw2', false);
+            }
+        }
     }]);
 
