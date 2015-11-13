1c6623109e0f95af0a10dde1e76a027beed54c82
Brandon Chen <brandonchen@Brandons-MacBook-Pro.local>
2015-11-13 00:25:47 -0800

    fix tests

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,40 +1,41 @@
 'use strict';
 
 describe('network sign up page', function(){
-
-    it('should throw an error on invalid email', function(){
+    beforeEach(function() {
         browser.get('http://localhost:8000/');
+    });
+    it('should be invalid on nonmatching pw', function(){
         var password1 = element(by.id('psw'));
         var password2 = element(by.id('psw2'));
         password1.sendKeys('test');
         password2.sendKeys('notthesameastheother');
-        var hint = element(by.css('.help-block'));
-        expect(hint.isPresent()).toEqual(false);
+        expect(element(by.id('psw2')).getAttribute('class')).toMatch('ng-invalid');
     });
 
-    it('should not throw an error on valid email', function(){
-        browser.get('http://localhost:8000/');
+    it('should be valid on matching pw', function(){
         var password1 = element(by.id('psw'));
         var password2 = element(by.id('psw2'));
         password1.sendKeys('same');
         password2.sendKeys('same');
-        var hint = element(by.css('.help-block'));
-        expect(hint.isPresent()).toEqual(true);
+        expect(element(by.id('psw2')).getAttribute('class')).toMatch('ng-valid');
     });
 
-    it('form should be invalid on invalid email', function(){
-        browser.get('http://localhost:8000/');
-        var emailinput = element(by.id('Email'));
+    it('should be invalid on invalid email', function(){
+        var email = element(by.id('Email'));
         email.sendKeys('missingat');
-        expect(hasClass(element(by.name('newForm')), 'ng-invalid')).toBe(true);
+        expect(element(by.name('newForm')).getAttribute('class')).toMatch('ng-invalid');
     });
 
-    it('form should be valid on valid email', function(){
-        browser.get('http://localhost:8000/');
-        var emailinput = element(by.id('Email'));
-        email.sendKeys('thereisan@here');
-        expect(hasClass(element(by.name('newForm')), 'ng-invalid')).toBe(false);
+    it('should be valid on valid email', function(){
+        var email = element(by.id('Email'));
+        email.sendKeys('thereisan@here.com');
+        expect(element(by.name('newForm')).getAttribute('class')).toMatch('ng-valid');
     });
+
+    it('should be invalid when nothing entered', function(){
+        expect(element(by.id('psw2')).getAttribute('class')).toMatch('ng-invalid');
+    });
+
 
 
 })
